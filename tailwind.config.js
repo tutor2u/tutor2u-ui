3e8ec885dd1ef02aca67f08fb68e0039f46f4ae7
Jamie Pittock <jamiepittock@gmail.com>
2020-11-20 17:11:47 +0000

    Updates defaults

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   presets: [require('tailwindcss/defaultConfig')],
   future: {
@@ -8,59 +10,43 @@ module.exports = {
   },
   purge: [],
   theme: {
+    fontFamily: {
+      sans: ['ClearSans','sans-serif'],
+    },
     fontWeight: {
       normal: '400',
       medium: '600',
       bold: '700',
     },
-    extend: {
-      fontFamily: {
-        sans: ['ClearSans','sans-serif'],
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: '#000',
+      white: '#fff',
+      gray: colors.coolGray,
+      bley: colors.blueGray,
+      red: colors.red,
+      pink: colors.pink,
+      blue: colors.lightBlue,
+      yellow: colors.amber,
+      green: colors.emerald,
+      social: {
+        email: '#69757c',
+        facebook: '#4a66a4',
+        linkedin: '#4374b4',
+        messenger: '#007fff',
+        pinterest: '#BD081C',
+        twitter: '#00aeef',
+        whatsapp: '#67c15e',
+        youtube: '#e33228'
       },
-      colors: {
-        'social': {
-          email: '#69757c',
-          facebook: '#4a66a4',
-          linkedin: '#4374b4',
-          messenger: '#007fff',
-          pinterest: '#BD081C',
-          twitter: '#00aeef',
-          whatsapp: '#67c15e',
-          youtube: '#e33228'
-        },
-        't2u-blue': {
-          lighter: '#0094de',
-          light: '#0072bb',
-          DEFAULT: '#0068a8',
-          dark: '#00498e',
-          darker: '#003971',
-        },
-        't2u-bley': {
-          'xx-light': '#f9fafb',
-          'x-light': '#f3f5f8',
-          'light': 'red',
-          DEFAULT: '#edeff2',
-          'dark': '#e2e6e8',
-          'x-dark': 'red',
-          'xx-dark': 'red',
-        },
-        't2u-pink': {
-          'xx-light': 'red',
-          'x-light': '#ed028c',
-          'light': 'red',
-          DEFAULT: '#ba006d',
-          'dark': 'red',
-          'x-dark': '#88004f',
-          'xx-dark': 'red',
-        },
-        't2u-subjects': {
-          business: '#00498e',
-          economics: '#822f66',
-          psychology: '#cd680d',
-          sociology: '#3f3f8c',
-          geography: '#2f8338',
-          politics: '#0d7f5a'
-        }
+      subjects: {
+        business: '#00498e',
+        economics: '#822f66',
+        psychology: '#cd680d',
+        sociology: '#3f3f8c',
+        geography: '#2f8338',
+        politics: '#0d7f5a'
       }
     },
   },
